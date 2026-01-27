1d12e2808a45b6c9f41ea0789f14267aec3c6a87
RyuuXiao <164801435+RyuuXiaoo@users.noreply.github.com>
2026-01-28 05:31:44 +0700

    Update db-nokos.js

--- db-nokos.js
+++ db-nokos.js
@@ -2,5 +2,5 @@
  {"number":"6283147485329","status":"active"},
  {"number":"6285213736323","status":"active"},
  {"number":"62895321055867","status":"active"},
- {"number":"6285180643962","status":"active"}]
+ {"number":"6285282615520","status":"active"}]
 
